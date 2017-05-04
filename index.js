2ff8247cb0fb1329817b5e3a33a5a004083d918d
Jake Crampton <jake.crampton@ossm.edu>
2017-05-04 15:44:05 -0500

    Added cross-side scripting protection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var sanitizeHtml = require('sanitize-html');
 
 // Routing
 app.use(express.static(__dirname + '/public'));
@@ -9,10 +10,18 @@ var io = require('socket.io')(server);
 io.on('connection', function(socket) {
 
     socket.on('message', function(data) {
+        var dirtyMessage = data.message;
+        var dirtyColor = data.color;
+        var dirtyName = data.name;
+
         console.log(data);
 
         // we tell the client to execute 'message'
-        socket.broadcast.emit('message', data);
+        socket.broadcast.emit('message', {
+            message: sanitizeHtml(dirtyMessage),
+            color: sanitizeHtml(dirtyColor),
+            name: sanitizeHtml(dirtyName)
+        });
     });
 });
 
